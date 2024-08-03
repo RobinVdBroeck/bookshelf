79cf3e8b98887b6c43a2e26c0e5fb0146e0cd4f7
Robin Van den Broeck <robin.broeck@gmail.com>
2024-08-03 14:52:35 +0200

    Setup the right env variables so we can connect to the postgres and mysql instance

---
+++ .github/workflows/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mysql: {
+    database: 'bookshelf_test',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    encoding: 'utf8',
+    host: 'mysql'
+  },
+
+  postgres: {
+    database: 'bookshelf_test',
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: 'postgres',
+    charset: 'utf8',
+    ssl: false
+  },
+
+  sqlite3: {
+    filename: ':memory:'
+  }
+};
